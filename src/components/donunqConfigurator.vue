bc186095bec2b86eaa4ac3cc74d06306fbcd169e
r0808 <arthurdelophem3@gmail.com>
2022-10-31 16:58:08 +0100

    feat: donunq configurator design overhaul

--- src/components/donunqConfigurator.vue
+++ src/components/donunqConfigurator.vue
@@ -16,22 +16,29 @@ export default {
 
 <template>
     <div class="configurePanel">
-        <div class="flavour">
-            <p class="flavour__title">choose your flavour: <span style="color:blue">{{ selectedFlavour }}</span></p>
-            <select v-model="selectedFlavour" v-on:change="emitDonunq" class="flavour__selector" name="flavours"
-                id="flavours">
-                <option selected value="chocolat">chocolat</option>
-                <option value="fraise">fraise</option>
-                <option value="myrtille">myrtille</option>
-            </select>
+        <div class="panelInfo">
+            <h1>Create your own donunq</h1>
+            <p><strong>let your imagination flow</strong></p>
         </div>
-        <div class="extras">
-            <p class="extras__title">want some extras: <span style="color:blue">{{ selectedExtra }}</span></p>
-            <select v-model="selectedExtra" v-on:change="emitDonunq" class="extras__selector" name="extras" id="extras">
-                <option selected value="boring">no I'm Boring</option>
-                <option value="vermicelles">vermicelles</option>
-                <option value="teknodisco">teknodisco</option>
-            </select>
+        <div class="panelInteraction">
+            <div class="flavour">
+                <p class="flavour__title">choose your flavour: <strong>{{ selectedFlavour }}</strong></p>
+                <select v-model="selectedFlavour" v-on:change="emitDonunq" class="flavour__selector" name="flavours"
+                    id="flavours">
+                    <option selected value="chocolat">chocolat</option>
+                    <option value="fraise">fraise</option>
+                    <option value="myrtille">myrtille</option>
+                </select>
+            </div>
+            <div class="extras">
+                <p class="extras__title">want some extras: <strong>{{ selectedExtra }}</strong></p>
+                <select v-model="selectedExtra" v-on:change="emitDonunq" class="extras__selector" name="extras"
+                    id="extras">
+                    <option selected value="boring">no I'm Boring</option>
+                    <option value="vermicelles">vermicelles</option>
+                    <option value="teknodisco">teknodisco</option>
+                </select>
+            </div>
         </div>
     </div>
 </template>
