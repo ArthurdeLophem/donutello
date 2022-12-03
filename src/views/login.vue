d17b42af9442bbf1076754b0aca002011b8aeb84
R0754975 <r0754975@student.thomasmore.be>
2022-12-03 13:18:34 +0100

    feat: created base files for login page

---
+++ src/views/login.vue
@@ -0,0 +1,8 @@
+<script setup>
+import lHeader from '../components/loginHeader.vue'
+import lForm from '../components/LoginForm.vue'
+</script>
+<template>
+    <lHeader />
+    <lForm />
+</template>
