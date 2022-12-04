f7a96adcc5faa0601a2387934b29b7cbee30d11b
R0754975 <r0754975@student.thomasmore.be>
2022-12-04 15:43:02 +0100

    feat: import newlymade dashboard and login

--- src/router.js
+++ src/router.js
@@ -1,14 +1,15 @@
 import { createWebHistory, createRouter } from "vue-router";
 import Home from './views/index.vue'
-import Gallery from './views/gallery.vue'
 import Generator from './views/generator.vue'
 import Login from './views/login.vue'
+import Dashboard from './views/dashboard.vue'
 
 const routes = [
     { path: "/", name: "Home", component: Home },
-    { path: "/gallery", name: "Gallery", component: Gallery },
     { path: "/generator", name: "Generator", component: Generator },
-    { path: "/login", name: "Login", component: Login }
+    { path: "/login", name: "Login", component: Login },
+    { path: "/dashboard", name: "Dashboard", component: Dashboard },
+
 ]
 
 //router constructor
