0d3944eaea29689532ba566523aae78c7dbd3d78
R0754975 <r0754975@student.thomasmore.be>
2022-12-05 17:18:24 +0100

    feat(order) simplified order cards and add fetch request to get current orders

--- src/components/dashboard/orders.vue
+++ src/components/dashboard/orders.vue
@@ -1,249 +1,72 @@
-<script></script>
+<script setup>
+import { ref, reactive, onMounted } from 'vue';
+
+let orders = reactive({ data: [] });
+
+onMounted(() => {
+    
+    const apiUrl ="http://localhost:3000/api/v1/donut"
+    fetch(apiUrl , {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+        orders.data = data.data
+        console.log(orders.data);
+    })
+    .catch(error => console.log(error))
+    
+
+
+})
+
+
+
+
+</script>
 <template>
     <div class="dashboard">
-        <div class="dashboard__filter">
+        <div class="dashboard__orders">
+            <div class="dashboard__filter">
             <div class="filter__btn">
                 <span class="btn__text">sorty by</span>
                 <img class="btn__img" src="assets/filter.png" alt="down">
             </div>
         </div>
-        <div class="dashboard__orders">
-            <div class="orders__card">
-                <span class="card__text">Order: #324324</span>
-                <h3 class="card__title">KBC Brussels</h3>
+            <div class="orders__card" v-for="order in orders.data">
+                <span class="card__text">Order:</span>
+                <h3 class="card__title">{{order.contact.name}}</h3>
                 <span class="card__text">Request Date:</span>
                 <span class="card__text card__text--pink">04/12/22</span>
-                <span class="card__text">Company Phone:</span>
-                <span class="card__text card__text--pink">0287654443</span>
-                <span class="card__text">Company Email:</span>
-                <span class="card__text card__text--pink">kbc.live@gmail.com</span>
-                <span class="card__text">Company Address:</span>
-                <span class="card__text card__text--pink">Rue de la Science 23</span>
-                <span class="card__text card__text--pink card__text--border">1000 Bruxelles</span>
-                <div class="card__donutdrop">
-                    <span class="donutdrop__text">Donuts:</span>
-                    <img class="donutdrop__img" src="" alt="dropdown">
-                </div>
-                <div class="card__donuts card__donuts--hidden">
-                    <ul class="donuts__list">
-                        <li class="list__item">
-                            <span class="item__title">donut 1</span>
-                            <span class="item__text">glazing: Strawberry</span>
-                            <span class="item__text">filling: Vanilla</span>
-                            <span class="item__text">topping: Red Sprinkles</span>
-                            <span class="item__text">extra: Leo</span>
-                            <span class="item__view">qauntity: 20</span>
-                        </li>
-                    </ul>
-                    <span class="donuts__total">Total: 100 donuts</span>
-                </div>
-                <div class="card__logo">
-                    <span class="logo__shape">Shape: Square</span>
-                    <span class="logo__btn">download logo</span>
-                </div>
-                <span class="card__status">Order Status: In Progress</span>
-                <span class="card__btn">edit status</span>
-            </div>
-            <div class="orders__card">
-                <span class="card__text">Order: #324324</span>
-                <h3 class="card__title">KBC Brussels</h3>
-                <span class="card__text">Request Date:</span>
-                <span class="card__text card__text--pink">04/12/22</span>
-                <span class="card__text">Company Phone:</span>
-                <span class="card__text card__text--pink">0287654443</span>
-                <span class="card__text">Company Email:</span>
-                <span class="card__text card__text--pink">kbc.live@gmail.com</span>
-                <span class="card__text">Company Address:</span>
-                <span class="card__text card__text--pink">Rue de la Science 23</span>
-                <span class="card__text card__text--pink card__text--border">1000 Bruxelles</span>
-                <div class="card__donutdrop">
-                    <span class="donutdrop__text">Donuts:</span>
-                    <img class="donutdrop__img" src="" alt="dropdown">
-                </div>
-                <div class="card__donuts card__donuts--hidden">
-                    <ul class="donuts__list">
-                        <li class="list__item">
-                            <span class="item__title">donut 1</span>
-                            <span class="item__text">glazing: Strawberry</span>
-                            <span class="item__text">filling: Vanilla</span>
-                            <span class="item__text">topping: Red Sprinkles</span>
-                            <span class="item__text">extra: Leo</span>
-                            <span class="item__view">qauntity: 20</span>
-                        </li>
-                    </ul>
-                    <span class="donuts__total">Total: 100 donuts</span>
-                </div>
-                <div class="card__logo">
-                    <span class="logo__shape">Shape: Square</span>
-                    <span class="logo__btn">download logo</span>
-                </div>
-                <span class="card__status">Order Status: In Progress</span>
-                <span class="card__btn">edit status</span>
-            </div>
-            <div class="orders__card">
-                <span class="card__text">Order: #324324</span>
-                <h3 class="card__title">KBC Brussels</h3>
-                <span class="card__text">Request Date:</span>
-                <span class="card__text card__text--pink">04/12/22</span>
-                <span class="card__text">Company Phone:</span>
-                <span class="card__text card__text--pink">0287654443</span>
-                <span class="card__text">Company Email:</span>
-                <span class="card__text card__text--pink">kbc.live@gmail.com</span>
-                <span class="card__text">Company Address:</span>
-                <span class="card__text card__text--pink">Rue de la Science 23</span>
-                <span class="card__text card__text--pink card__text--border">1000 Bruxelles</span>
-                <div class="card__donutdrop">
-                    <span class="donutdrop__text">Donuts:</span>
-                    <img class="donutdrop__img" src="" alt="dropdown">
-                </div>
-                <div class="card__donuts card__donuts--hidden">
-                    <ul class="donuts__list">
-                        <li class="list__item">
-                            <span class="item__title">donut 1</span>
-                            <span class="item__text">glazing: Strawberry</span>
-                            <span class="item__text">filling: Vanilla</span>
-                            <span class="item__text">topping: Red Sprinkles</span>
-                            <span class="item__text">extra: Leo</span>
-                            <span class="item__view">qauntity: 20</span>
-                        </li>
-                    </ul>
-                    <span class="donuts__total">Total: 100 donuts</span>
-                </div>
-                <div class="card__logo">
-                    <span class="logo__shape">Shape: Square</span>
-                    <span class="logo__btn">download logo</span>
-                </div>
-                <span class="card__status">Order Status: In Progress</span>
-                <span class="card__btn">edit status</span>
-            </div>
-            <div class="orders__card">
-                <span class="card__text">Order: #324324</span>
-                <h3 class="card__title">KBC Brussels</h3>
-                <span class="card__text">Request Date:</span>
-                <span class="card__text card__text--pink">04/12/22</span>
-                <span class="card__text">Company Phone:</span>
-                <span class="card__text card__text--pink">0287654443</span>
-                <span class="card__text">Company Email:</span>
-                <span class="card__text card__text--pink">kbc.live@gmail.com</span>
-                <span class="card__text">Company Address:</span>
-                <span class="card__text card__text--pink">Rue de la Science 23</span>
-                <span class="card__text card__text--pink card__text--border">1000 Bruxelles</span>
-                <div class="card__donutdrop">
-                    <span class="donutdrop__text">Donuts:</span>
-                    <img class="donutdrop__img" src="" alt="dropdown">
-                </div>
-                <div class="card__donuts card__donuts--hidden">
-                    <ul class="donuts__list">
-                        <li class="list__item">
-                            <span class="item__title">donut 1</span>
-                            <span class="item__text">glazing: Strawberry</span>
-                            <span class="item__text">filling: Vanilla</span>
-                            <span class="item__text">topping: Red Sprinkles</span>
-                            <span class="item__text">extra: Leo</span>
-                            <span class="item__view">qauntity: 20</span>
-                        </li>
-                    </ul>
-                    <span class="donuts__total">Total: 100 donuts</span>
-                </div>
-                <div class="card__logo">
-                    <span class="logo__shape">Shape: Square</span>
-                    <span class="logo__btn">download logo</span>
-                </div>
-                <span class="card__status">Order Status: In Progress</span>
-                <span class="card__btn">edit status</span>
-            </div>
-            <div class="orders__card">
-                <span class="card__text">Order: #324324</span>
-                <h3 class="card__title">KBC Brussels</h3>
-                <span class="card__text">Request Date:</span>
-                <span class="card__text card__text--pink">04/12/22</span>
-                <span class="card__text">Company Phone:</span>
-                <span class="card__text card__text--pink">0287654443</span>
-                <span class="card__text">Company Email:</span>
-                <span class="card__text card__text--pink">kbc.live@gmail.com</span>
-                <span class="card__text">Company Address:</span>
-                <span class="card__text card__text--pink">Rue de la Science 23</span>
-                <span class="card__text card__text--pink card__text--border">1000 Bruxelles</span>
-                <div class="card__donutdrop">
-                    <span class="donutdrop__text">Donuts:</span>
-                    <img class="donutdrop__img" src="" alt="dropdown">
-                </div>
-                <div class="card__donuts card__donuts--hidden">
-                    <ul class="donuts__list">
-                        <li class="list__item">
-                            <span class="item__title">donut 1</span>
-                            <span class="item__text">glazing: Strawberry</span>
-                            <span class="item__text">filling: Vanilla</span>
-                            <span class="item__text">topping: Red Sprinkles</span>
-                            <span class="item__text">extra: Leo</span>
-                            <span class="item__view">qauntity: 20</span>
-                        </li>
-                    </ul>
-                    <span class="donuts__total">Total: 100 donuts</span>
-                </div>
-                <div class="card__logo">
-                    <span class="logo__shape">Shape: Square</span>
-                    <span class="logo__btn">download logo</span>
-                </div>
-                <span class="card__status">Order Status: In Progress</span>
-                <span class="card__btn">edit status</span>
-            </div>
-            <div class="orders__card">
-                <span class="card__text">Order: #324324</span>
-                <h3 class="card__title">KBC Brussels</h3>
-                <span class="card__text">Request Date:</span>
-                <span class="card__text card__text--pink">04/12/22</span>
-                <span class="card__text">Company Phone:</span>
-                <span class="card__text card__text--pink">0287654443</span>
-                <span class="card__text">Company Email:</span>
-                <span class="card__text card__text--pink">kbc.live@gmail.com</span>
-                <span class="card__text">Company Address:</span>
-                <span class="card__text card__text--pink">Rue de la Science 23</span>
-                <span class="card__text card__text--pink card__text--border">1000 Bruxelles</span>
-                <div class="card__donutdrop">
-                    <span class="donutdrop__text">Donuts:</span>
-                    <img class="donutdrop__img" src="" alt="dropdown">
-                </div>
-                <div class="card__donuts card__donuts--hidden">
-                    <ul class="donuts__list">
-                        <li class="list__item">
-                            <span class="item__title">donut 1</span>
-                            <span class="item__text">glazing: Strawberry</span>
-                            <span class="item__text">filling: Vanilla</span>
-                            <span class="item__text">topping: Red Sprinkles</span>
-                            <span class="item__text">extra: Leo</span>
-                            <span class="item__view">qauntity: 20</span>
-                        </li>
-                    </ul>
-                    <span class="donuts__total">Total: 100 donuts</span>
-                </div>
-                <div class="card__logo">
-                    <span class="logo__shape">Shape: Square</span>
-                    <span class="logo__btn">download logo</span>
-                </div>
-                <span class="card__status">Order Status: In Progress</span>
-                <span class="card__btn">edit status</span>
+                <router-link :to="('/generator?orderid=' + order._id)" class="card__btn">details</router-link>
             </div>
         </div>
-    </div>
+    </div>  
 </template>
 <style scoped>
 
 .dashboard {
-    width: 100vw;
+    width: 100%;
     height: 100vh;
     background-image: url('https://donuttello.com/photos/shares/donuts/sinterklaas/sint_2022.jpg');
     background-repeat: no-repeat;
     background-size: cover;
+    background-position: center;
+    background-attachment:fixed;
+    overflow-y: hidden;
+
 }
-
-
 .dashboard__filter {
-    width: 100vw;
+    width: 100%;
     height: 8vh;
     display: flex;
     justify-content: flex-start;
     align-items: center;
+    margin-top: 3vh;
     
 }
 .filter__btn {
@@ -254,6 +77,7 @@
     width: 10rem;
     height: 3rem;
     margin: 0 2em;
+    border-radius: 5px;
 }
 
 .btn__text {
@@ -263,34 +87,66 @@
 }
 
 .dashboard__orders {
-    width: 100vw;
-    height: 87vh;
+    width: 100%;
+    height: 100%;
     display: flex;
     flex-direction: row;
     justify-content: space-evenly;
     flex-wrap: wrap;
+    box-sizing: border-box;
+    gap: 3rem;
+    overflow-y: auto;
+    scrollbar-width: none;
+    padding: 0 2rem 3vh 2rem;
+}
 
+.dashboard__orders::-webkit-scrollbar {
+  display: none;
 }
 
 .orders__card {
     background-color: white;
     display: flex;
     flex-direction: column;
-    width: 20vw;
-    height: 60vh;
+    width: 12.5%;
+    height: fit-content;
     padding: 2rem 2rem;
     border-radius: 20px;
-    margin: 2rem 0;
     box-shadow: 0px 5px 7px -2px rgba(0,0,0,0.5);
+    box-sizing: border-box;
+    
 }
 
-.list__item {
-    display: flex;
-    flex-direction: column;
+
+.card__text {
+    font-size: 1.2rem;
+    font-weight: 600;
+    margin: 0.5rem 0;
 }
 
-.card__donuts--hidden {
-    display: none;
+.card__text--pink {
+    color: #ed2970;
+    margin-bottom: 1rem;
+}
+
+.card__title {
+    font-size: 1.75rem;
+    font-weight: 600;
+    margin: 0.5rem 0;
+    word-wrap: break-word;
+}
+
+.card__btn {
+    background-color: #ed2970;
+    color: white;
+    font-size: 1.5rem;
+    width: 100%;
+    height: 3rem;
+    font-weight: 600;
+    display:flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 10px;
 }
 
 </style>   
