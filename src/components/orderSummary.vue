fc3638e06bebfb81c744191d77a619dcea954089
r0808 <arthurdelophem3@gmail.com>
2022-12-07 13:45:47 +0100

    feat: add donut order card frontend

--- src/components/orderSummary.vue
+++ src/components/orderSummary.vue
@@ -6,20 +6,30 @@
     <div class="summary__container" ref="extra__container">
         <h2 class="summary__Headline"><strong>Your</strong> Donuts</h2>
         <div class="summary__list">
-            <div class="extra__block" ref="selector">
-                <div class="donut__position">1</div>
-                <div class="donut__data">
-                    <div class="topping__data">
-
+            <div class="donut__block" ref="selector">
+                <div class="donut__position">
+                    <p>1.</p>
+                </div>
+                <div class="donut__card">
+                    <div class="card__col">
+                        <div class="data__container">
+                            <p>topping</p>
+                            <p><strong>marshmallow</strong></p>
+                        </div>
+                        <div class="data__container">
+                            <p>extra</p>
+                            <p><strong>marshmallow</strong></p>
+                        </div>
                     </div>
-                    <div class="extra__data">
-
-                    </div>
-                    <div class="vermi__data">
-
-                    </div>
-                    <div class="campaign__data">
-
+                    <div class="card__col">
+                        <div class="data__container">
+                            <p>sprinkles</p>
+                            <p><strong>marshmallow</strong></p>
+                        </div>
+                        <div class="data__container">
+                            <p>campaign size</p>
+                            <p><strong>50</strong></p>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -32,7 +42,46 @@ strong {
     color: #ed2970;
 }
 
+p {
+    font-weight: bold;
+}
+
+.summary__list {
+    width: 50%;
+    margin-left: 2em;
+}
+
 .summary__Headline {
     font-size: 2.4em;
+}
+
+.donut__card {
+    background-color: #EBEBEB;
+    border-radius: 5px;
+    display: flex;
+    flex-direction: row;
+    padding: 1em 4em;
+    justify-content: space-around;
+    gap: 3em;
+}
+
+.donut__block {
+    display: flex;
+    flex-direction: row;
+    gap: 2em;
+}
+
+.donut__position {
+    background-color: #EBEBEB;
+    border-radius: 5px;
+    width: fit-content;
+    height: fit-content;
+    padding: 0 20px;
+}
+
+.data__container {
+    display: flex;
+    flex-direction: row;
+    gap: 1em;
 }
 </style>
