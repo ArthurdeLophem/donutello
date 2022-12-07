e3cbf1cb38f97884f1c78c65884673312a6f1a87
timvernaeve <r0754975@student.thomasmore.be>
2022-12-07 13:36:04 +0100

    feat(dashboard): redirect to login if not logged in

--- src/views/dashboard.vue
+++ src/views/dashboard.vue
@@ -1,6 +1,28 @@
 <script setup>
 import Dheader from '../components/dashboard/header.vue'
 import Dorders from '../components/dashboard/orders.vue'
+import { ref, reactive, onMounted } from 'vue';
+
+onMounted(() => {
+    const authUrl = 'http://localhost:3000/api/v1/users/auth'
+fetch(authUrl, {
+    method: 'POST',
+    headers: {
+       "Authorization" : "Bearer " + localStorage.getItem('token'),
+    }
+})
+.then(res => {
+
+if(res.status == 401) {
+    window.location.href = '/login';
+ }
+   
+}).catch(error => {
+    console.log(error)
+});
+
+})
+
 </script>
 <template>
     <Dheader />
