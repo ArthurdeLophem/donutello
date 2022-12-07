91f37f1559a4b26675041c06813e7a30368b0cab
r0808 <arthurdelophem3@gmail.com>
2022-12-07 10:08:01 +0100

    feat: add order panel to vue router

--- src/router.js
+++ src/router.js
@@ -1,15 +1,16 @@
 import { createWebHistory, createRouter } from "vue-router";
 import Home from './views/index.vue'
 import Generator from './views/generator.vue'
+import OrderPanel from './views/orderPanel.vue'
 import Login from './views/login.vue'
 import Dashboard from './views/dashboard.vue'
 
 const routes = [
     { path: "/", name: "Home", component: Home },
     { path: "/generator", name: "Generator", component: Generator },
+    { path: "/order", name: "order", component: OrderPanel },
     { path: "/login", name: "Login", component: Login },
     { path: "/dashboard", name: "Dashboard", component: Dashboard },
-
 ]
 
 //router constructor
