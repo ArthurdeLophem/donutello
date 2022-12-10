5ab458f2cc0a91c0f2fb0731ab6afc8863ec63b9
r0808 <arthurdelophem3@gmail.com>
2022-12-10 18:14:28 +0100

    fix: rm signup + add update url paths

--- src/configs/config.js
+++ src/configs/config.js
@@ -1,5 +1,4 @@
 export const loginUrl = 'http://localhost:3000/api/v1/users/login',
-    signupUrl = 'http://localhost:3000/api/v1/users/signup',
     authUrl = 'http://localhost:3000/api/v1/users/auth',
+    updateUrl = 'http://localhost:3000/api/v1/users/update',
     baseDonutUrl = 'http://localhost:3000/api/v1/donut'
-
