6798b471cf36b5f8b61ee57af24c8037cfe24426
r0808 <arthurdelophem3@gmail.com>
2022-12-10 12:58:04 +0100

    feat: add new flavour to donuttelloData config

--- src/configs/donuttelloData.js
+++ src/configs/donuttelloData.js
@@ -19,30 +19,94 @@ export const extrasData = [
 
 export const toppingsData = [
     {
-        color: "#c51d34",
-        eName: "framboos"
-    },
-    {
-        color: "#d2691e",
+        color: "#6c3b1e",
         eName: "choco"
     },
     {
-        color: "#000000",
-        eName: "darkk"
+        color: "#95c58e",
+        eName: "pistache"
+    },
+    {
+        color: "#ff3857",
+        eName: "cherry"
+    },
+    {
+        color: "#adf802",
+        eName: "green lemon"
+    },
+    {
+        color: "#ddd0be",
+        eName: "cocos"
+    },
+    {
+        color: "#bb9568",
+        eName: "camel"
+    },
+    {
+        color: "#d53032",
+        eName: "strawberry"
+    },
+    {
+        color: "#6c3b1e",
+        eName: "nuttella"
+    },
+    {
+        color: "#f6da73",
+        eName: "banana"
+    },
+    {
+        color: "#5f322d",
+        eName: "brownie"
+    },
+    {
+        color: "#924f28",
+        eName: "speculoos"
     }
 ]
 
 export const vermisData = [
     {
-        color: "#c51d34",
-        eName: "framboos"
-    },
-    {
         color: "#d2691e",
         eName: "choco"
     },
     {
-        color: "#000000",
-        eName: "darkk"
+        color: "#95c58e",
+        eName: "pistache"
+    },
+    {
+        color: "#adf802",
+        eName: "green lemon"
+    },
+    {
+        color: "#6c3b1e",
+        eName: "nuttella"
+    },
+    {
+        color: "#924f28",
+        eName: "speculoos"
+    },
+    {
+        color: "#1b1811",
+        eName: "black choco"
+    },
+    {
+        color: "#ede9e0",
+        eName: "marshmallow"
+    },
+    {
+        color: "#f0e3c7",
+        eName: "white choco"
+    },
+    {
+        color: "#f89c37",
+        eName: "caramel"
+    },
+    {
+        color: "#f89c37",
+        eName: "salted caramel"
+    },
+    {
+        color: "#8c766d",
+        eName: "cappuccino"
     }
 ]
