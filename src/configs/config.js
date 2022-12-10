a4296c975b2619bda1877855b81d8907c114784c
r0808 <arthurdelophem3@gmail.com>
2022-12-10 15:34:11 +0100

    docs: move api url to api config file

---
+++ src/configs/config.js
@@ -0,0 +1,5 @@
+export const loginUrl = 'http://localhost:3000/api/v1/users/login',
+    signupUrl = 'http://localhost:3000/api/v1/users/signup',
+    authUrl = 'http://localhost:3000/api/v1/users/auth',
+    baseDonutUrl = 'http://localhost:3000/api/v1/donut'
+
