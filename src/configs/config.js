6975a85482c23a8ebbbcc5a4f36890f9ad71261a
R0754975 <r0754975@student.thomasmore.be>
2022-12-11 21:25:05 +0100

    feat: moved config to root and add to gitignore

--- src/configs/config.js
+++
@@ -1,4 +0,0 @@
-export const loginUrl = 'http://localhost:3000/api/v1/users/login',
-    authUrl = 'http://localhost:3000/api/v1/users/auth',
-    updateUrl = 'http://localhost:3000/api/v1/users/update',
-    baseDonutUrl = 'http://localhost:3000/api/v1/donut'
