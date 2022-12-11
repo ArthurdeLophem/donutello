c70abd7f253e339e97edd33c65913bf918261e2e
R0754975 <r0754975@student.thomasmore.be>
2022-12-11 17:27:56 +0100

    feat(details): add components to view

--- src/views/details.vue
+++ src/views/details.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { ref, reactive, onMounted } from 'vue';
 import Dheader from '../components/dashboard/header.vue'
+import summaryDetails from '../components/details/summaryDetails.vue'
 import orderDetails from '../components/details/orderDetails.vue'
 
 onMounted(() => {
@@ -27,8 +28,22 @@ if(res.status == 401) {
 <template>
 <Dheader />
     <div class="content__container">
+        <summaryDetails />
         <orderDetails />
     </div>
 </template>
 <style scoped>
+
+.content__container {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+    padding: 0 5rem;
+    margin-top: 5rem;
+    gap: 10rem;
+}
+
 </style>
