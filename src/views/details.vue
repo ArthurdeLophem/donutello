40cb19f0c7148f0a3b0105426a0b8616aa60ed97
R0754975 <r0754975@student.thomasmore.be>
2022-12-11 17:14:29 +0100

    feat: created and linked new details view

---
+++ src/views/details.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { ref, reactive, onMounted } from 'vue';
+import Dheader from '../components/dashboard/header.vue'
+import orderDetails from '../components/details/orderDetails.vue'
+
+onMounted(() => {
+    const authUrl = 'http://localhost:3000/api/v1/users/auth'
+fetch(authUrl, {
+    method: 'POST',
+    headers: {
+       "Authorization" : "Bearer " + localStorage.getItem('token'),
+    }
+})
+.then(res => {
+
+if(res.status == 401) {
+    window.location.href = '/login';
+ }
+   
+}).catch(error => {
+    console.log(error)
+});
+
+})
+
+</script>
+<template>
+<Dheader />
+    <div class="content__container">
+        <orderDetails />
+    </div>
+</template>
+<style scoped>
+</style>
