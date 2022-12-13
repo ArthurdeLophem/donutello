118b678aad4c4b9a7ed44271ada874e1ab2a93c4
r0808 <arthurdelophem3@gmail.com>
2022-12-13 09:38:03 +0100

    fix: generator route is back

--- src/router.js
+++ src/router.js
@@ -12,7 +12,7 @@ import Overview from './views/overview.vue'
 const routes = [
     { path: "/", name: "Home", component: Home },
     { path: "/previewer", name: "Previewer", component: Previewer },
-    { path: "/order", name: "Order", component: OrderPanel },
+    { path: "/generator", name: "Generator", component: Generator },
     { path: "/order", name: "Order", component: OrderPanel },
     { path: "/login", name: "Login", component: Login },
     { path: "/dashboard", name: "Dashboard", component: Dashboard },
