cd69918e2132967a803dc38d5f8fadaf33c71965
r0808 <arthurdelophem3@gmail.com>
2023-06-24 11:16:41 +0200

    fix: generator

--- src/views/generator.vue
+++ src/views/generator.vue
@@ -2,7 +2,6 @@
 import donutConfigurator from '../components/genny/generatorNav.vue';
 import donutViewport from '../components/genny/threeViewport.vue';
 import configPanels from '../components/genny/configurePanels.vue';
-import { baseDonutUrl } from '../../config';
 import { reactive } from 'vue';
 
 const urlParams = new URLSearchParams(window.location.search),
@@ -13,21 +12,6 @@ let donutData = reactive({ data: [] }),
     editDonut = JSON.parse(window.localStorage.getItem("editor")),
     donutType = reactive({ type: "" });
 
-if (donutId && orderId && token) {
-    fetch(baseDonutUrl + "/" + orderId + "/" + donutId, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-        }
-    }).then(response =>
-        response.json()
-    ).then(data => {
-        donutData.data = data.data
-        donutType.type = "fetch"
-    }).catch(
-        error => console.log(error)
-    )
-} else {
     setTimeout(() => {
         if (editDonut) {
             donutData.data = editDonut
@@ -38,7 +22,6 @@ if (donutId && orderId && token) {
             donutType.type = "fresh"
         }
     }, 100);
-}
 </script>
 
 <template>
