cbc19a6e8c27241fde517429dd8f975abf05e77c
r0808 <arthurdelophem3@gmail.com>
2023-06-24 11:25:49 +0200

    fix: errors

--- src/router.js
+++ src/router.js
@@ -4,32 +4,20 @@ import {
 } from "vue-router";
 import Home from './views/index.vue'
 import Generator from './views/generator.vue'
-import Previewer from './views/previewer.vue'
-import Mobile from './views/mobile.vue'
 import redirect from './middleware/redirect.js'
 
-const routes = [{
-    path: "/",
-    name: "Home",
-    component: Home,
-},
-{
-    path: "/previewer",
-    name: "Previewer",
-    component: Previewer,
-    beforeEnter: redirect
-},
-{
-    path: "/generator",
-    name: "Generator",
-    component: Generator,
-    beforeEnter: redirect
-},
-{
-    path: "/mobile",
-    name: "Mobile",
-    component: Mobile,
-}
+const routes = [
+    {
+        path: "/",
+        name: "Home",
+        component: Home,
+    },
+    {
+        path: "/generator",
+        name: "Generator",
+        component: Generator,
+        beforeEnter: redirect
+    }
 ]
 
 //router constructor
